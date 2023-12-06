d2c5974d96356a69e7475f2937b0d9635149d79e
shetalw <sdwaters8@gmail.com>
2023-12-06 08:35:46 -0500

    fixed modules layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import HelloWorld from "./Labs/a3/HelloWorld";
 import Kanbas from "./Kanbas";
 import {HashRouter} from "react-router-dom";
 import {Routes, Route, Navigate} from "react-router";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
 function App() {
--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import db from "../../Database";
-
-import { FaEllipsisV, FaRegCheckCircle, FaPlus, FaRegTrashAlt, FaRegEdit } from "react-icons/fa"
+import { FaEllipsisV, FaRegCheckCircle, FaPlus, FaRegTrashAlt, FaRegEdit } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
+import './moduleList.css';
 
 import {
   addModule,
@@ -12,126 +12,126 @@ import {
   setModule,
   setModules,
 } from "./modulesReducer";
-import * as client from "../client";
-// import { findModulesForCourse, createModule, deleteModule } from "../client";
+
+
+
 
 function ModuleList() {
   const { courseId } = useParams();
-
-  // get all module
-  useEffect(() => {
-    client.findModulesForCourse(courseId)
-      .then((modules) =>
-        dispatch(setModules(modules))
-      );
-  }, [courseId]);
-
-
+  const dispatch = useDispatch();
   const modules = useSelector((state) => state.modulesReducer.modules);
   const module = useSelector((state) => state.modulesReducer.module);
-  const dispatch = useDispatch();
-
-  // whether to show add module
   const [showAddModule, setShowAddModule] = useState(false);
 
-  // async
-  const handleAddModule = () => {
-    client.createModule(courseId, module).then((module) => {
-      dispatch(addModule(module));
-    });
+  // Fetch modules for a course
+  useEffect(() => {
+    const fetchModules = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_API_BASE}/courses/${courseId}/modules`);
+        dispatch(setModules(response.data));
+      } catch (error) {
+        console.error("Error fetching modules:", error);
+      }
+    };
+    fetchModules();
+  }, [courseId, dispatch]);
+
+  // Add a module
+  const handleAddModule = async () => {
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_API_BASE}/courses/${courseId}/modules`, module);
+      dispatch(addModule(response.data));
+    } catch (error) {
+      console.error("Error adding module:", error);
+    }
   };
 
-  // async
-  const handleDeleteModule = (moduleId) => {
-    client.deleteModule(moduleId).then((status) => {
+  // Delete a module
+  const handleDeleteModule = async (moduleId) => {
+    try {
+      await axios.delete(`${process.env.REACT_APP_API_BASE}/modules/${moduleId}`);
       dispatch(deleteModule(moduleId));
-    });
+    } catch (error) {
+      console.error("Error deleting module:", error);
+    }
   };
 
+  // Update a module
   const handleUpdateModule = async () => {
-    const status = await client.updateModule(module);
-    dispatch(updateModule(module));
+    try {
+      await axios.put(`${process.env.REACT_APP_API_BASE}/modules/${module._id}`, module);
+      dispatch(updateModule(module));
+    } catch (error) {
+      console.error("Error updating module:", error);
+    }
   };
 
   return (
     <>
-      <div className="d-flex flex-row wd-main-content">
-        <div className="row"></div>
-        <div class="row wd-main-content">
-          <div class="col-12 wd-button-bar">
-            <button class="btn wd-btn-gray float-end">< FaEllipsisV /></button>
-            <button class="btn wd-btn-red float-end" onClick={() => setShowAddModule(!showAddModule)}><FaPlus /> Module</button>
-            <div class="dropdown float-end">
-              <button class="btn wd-btn-gray dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                <FaRegCheckCircle className="wd-icon-green" /> Publish
-                All
+      <div className="module-container">
+        <div className="row wd-main-content">
+          <div className="col-12 wd-button-bar">
+            <button className="btn wd-btn-gray float-end"><FaEllipsisV /></button>
+            <button className="btn wd-btn-red" onClick={() => setShowAddModule(!showAddModule)}><FaPlus /> Module</button>
+            <div className="dropdown float-end">
+              <button className="btn wd-btn-gray dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <FaRegCheckCircle className="wd-icon-green" /> Publish All
               </button>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Publish All</a></li>
-                <li><a class="dropdown-item" href="#">Publish All Items and Modules</a></li>
-                <li><a class="dropdown-item" href="#">Unpublish</a></li>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Publish All</a></li>
+                <li><a className="dropdown-item" href="#">Publish All Items and Modules</a></li>
+                <li><a className="dropdown-item" href="#">Unpublish</a></li>
               </ul>
             </div>
-            <button class="btn wd-btn-gray float-end">View Progress</button>
-            <button class="btn wd-btn-gray float-end" onclick="window.location.href='#';">Collapse
-              All</button>
+            <button className="btn wd-btn-gray">View Progress</button>
+            <button className="btn wd-btn-gray" onClick={() => {/* Collapse All logic */}}>Collapse All</button>
           </div>
 
-          {
-            modules
-              .filter((module) => module.course === courseId)
-              .map((module, index) => (
-                <ul className="list-group wd-course-ul">
-                  <li key={index} className="list-group-item list-group-item-secondary">
-                    <div className="wd-module-title">
-                      {module.name}
-                      <button className="float-end btn wd-btn-transparent-module"
-                        onClick={() => handleDeleteModule(module._id)}>
-                        <FaRegTrashAlt />
-                      </button>
-                      <button className="float-end btn wd-btn-transparent-module"
-                        onClick={() => {
-                          setShowAddModule(true);
-                          dispatch(setModule(module))
-                        }}>
-                        <FaRegEdit />
-                      </button>
-                    </div>
-                  </li>
-                  <li key={index} className="list-group-item">
-                    <p>{module.description}</p>
-                  </li>
-                </ul>
-              ))
+          {modules
+            .filter((mod) => mod.course === courseId)
+            .map((mod, index) => (
+              <div key={index} className="list-group">
+                <div className="list-group-item list-group-item-secondary">
+                  <div className="wd-module-title">
+                    {mod.name}
+                    <button className="float-end btn wd-btn-transparent-module" onClick={() => handleDeleteModule(mod._id)}>
+                      <FaRegTrashAlt />
+                    </button>
+                    <button className="float-end btn wd-btn-transparent-module" onClick={() => {setShowAddModule(true); dispatch(setModule(mod))}}>
+                      <FaRegEdit />
+                    </button>
+                  </div>
+                </div>
+                <div className="list-group-item">
+                  <p>{mod.description}</p>
+                </div>
+              </div>
+            ))
           }
         </div>
-        {showAddModule && <div className="wd-add-module">
-     
+         {showAddModule && (
+        <div className="wd-add-module">
           Module Name
           <input
-            value={module.name} placeholder="Module Name"
-            onChange={(e) =>
-              dispatch(setModule({ ...module, name: e.target.value }))
-            } />
+            value={module.name} // Replaced moduleDetails with module
+            placeholder="Module Name"
+            onChange={(e) => dispatch(setModule({ ...module, name: e.target.value }))}
+          />
           <br />
           Description
           <br />
           <textarea
-            value={module.description} placeholder="Description"
-            onChange={(e) =>
-              dispatch(setModule({ ...module, description: e.target.value }))
-            } />
-          <button className="btn wd-btn-gray"
-            onClick={handleAddModule}>
-            Add
-          </button>
-          <button className="btn wd-btn-gray"
-            onClick={handleUpdateModule}>
-            Update
-          </button>
-        </div>}
+            value={module.description} // Replaced moduleDetails with module
+            placeholder="Description"
+            onChange={(e) => dispatch(setModule({ ...module, description: e.target.value }))}
+          />
+          <button className="btn wd-btn-gray" onClick={handleAddModule}>Add</button>
+          <button className="btn wd-btn-gray" onClick={handleUpdateModule}>Update</button>
+        </div>
+        )}
       </div>
-    </>);
+    </>
+  );
 }
+
 export default ModuleList;
